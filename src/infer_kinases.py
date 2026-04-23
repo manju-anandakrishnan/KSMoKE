890c186899edeb8db744c6dbdb230322cdf43255
ManjuA <manjua@udel.edu>
2026-04-23 17:54:56 -0400

    Updating instructions for file uploads

--- src/infer_kinases.py
+++ src/infer_kinases.py
@@ -34,7 +34,7 @@ with st.container(border=True):
     # File upload section    
     st.markdown("""Upload a CSV file with at least two columns - protein and site.<br/>
                     You file must include a header line with labels - protein, site. <br/>
-                    Protein must be a UniProt Accession Number. <br/>
+                    Protein must be UniProt Accession Number. It must be a reviewed entry for organsim, 'Human'. <br/>
                     The column, 'site' must be of the format \<residue\>\<position\>. For example: Y1135, T202, Y1000 <br/>
                     Optionally, include logFC and p-value and choose thresholds for the two columns. <br/>
                     All other columns in the phosphoproteome file will be ignored. <br/>""",unsafe_allow_html=True)
@@ -92,7 +92,7 @@ with st.container(border=True):
         st.markdown("""<b>Custom library format specifications:</b><br/>
                     Upload a background file (CSV format with the mandatory columns: kinase, substrate, site). <br/>
                     You file must include a header line with labels - kinase, substrate, site. <br/>
-                    The values for kinase and substrate must be UniProt Accession Numbers of the proteins.<br/>
+                    The values for kinase and substrate must be UniProt Accession Numbers of the human proteins. The UniProt Accession Numbers must be reviewed entries in UniProt<br/>
                     The column, 'site' must be of the format \<residue\>\<position\>. For example: Y1135, T202, Y1000 <br/>
                     All other columns in the background file will be ignored.<br/>""",unsafe_allow_html=True)
                                         
